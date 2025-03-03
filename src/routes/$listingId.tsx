811eba58cf691afde5597de796da9ead3f3eb1b9
Ernie1234 <anumahjoshuaeneye@gmail.com>
2025-03-03 12:52:57 +0100

    Enhance listing detail route with error handling and improved UI; add retry functionality and display listing information

--- src/routes/$listingId.tsx
+++ src/routes/$listingId.tsx
@@ -1,5 +1,5 @@
 import { getListingById } from "@/services/listings";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/$listingId")({
   component: RouteComponent,
@@ -9,10 +9,47 @@ export const Route = createFileRoute("/$listingId")({
       listing,
     };
   },
+  onError: ({ error }) => {
+    console.error(error);
+  },
+  errorComponent: ({ error, reset }) => {
+    const router = useRouter();
+
+    return (
+      <div>
+        {error.message}
+        <button
+          onClick={() => {
+            // Invalidate the route to reload the loader, which will also reset the error boundary
+            router.invalidate();
+          }}
+        >
+          retry
+        </button>
+      </div>
+    );
+  },
 });
 
 function RouteComponent() {
   const { listing } = Route.useLoaderData();
   console.log("Listing in listing details: ", listing);
-  return <div>Hello "/$listingId"!</div>;
+  return (
+    <div className="min-h-dvh">
+      <div className="lg:max-w-10/12 p-5 mx-auto">
+        <img
+          src={listing.data.imageSrc}
+          alt={listing.data.title}
+          className="rounded-xl w-full "
+        />
+        <div className="mt-5">
+          <h1>{listing.data.title}</h1>
+          <p>{listing.data.description}</p>
+          <p>Price: ${listing.data.price}</p>
+          <p>Location: {listing.data.location}</p>
+          <p>Host: {listing.data.hostName}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
