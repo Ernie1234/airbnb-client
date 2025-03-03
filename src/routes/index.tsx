17f5a5cf6001a9e6f849c8f01420011ea1a42d96
Ernie1234 <anumahjoshuaeneye@gmail.com>
2025-03-03 11:15:32 +0100

    Implement listing display with loading and error states; add ListingSkeleton and Card components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,47 @@
+import { ListingSkeleton } from "@/components/shared/ListingSkeleton";
+import { Card, CardContent } from "@/components/ui/card";
+import { getAllListing } from "@/services/listings";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { Heart } from "lucide-react";
 
 export const Route = createFileRoute("/")({
   component: App,
 });
 
 function App() {
-  return <div className="text-red-400 text-4xl font-bold">hello world</div>;
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["getAllListing"],
+    queryFn: async () => await getAllListing(),
+  });
+
+  if (isLoading) return <ListingSkeleton />;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return <div>No listings found.</div>;
+  console.log("Listing: ", data);
+
+  return (
+    <div className="text-4xl font-bold p-5">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {data.data.map((listing: any) => (
+          <div className="w-[350px] relative group" key={listing.id}>
+            <Heart className="absolute top-2 right-2 text-gray-200 hover:scale-110 duration-300 transition-all fill-gray-400" />
+            <img
+              src={listing.imageSrc}
+              alt={listing.title}
+              className="min-w-[280px] min-h-[280px] object-cover w-full h-auto rounded-xl"
+            />
+            <div className="">
+              <p className="text-xl font-semibold">{listing.title}</p>
+              <p className="text-sm font-light">{listing.description}</p>
+              <p className="font-medium text-base">
+                ${listing.price}{" "}
+                <span className="text-sm font-light">night</span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/services/listings.ts
@@ -0,0 +1,18 @@
+import axiosInstance from "./axiosInstance";
+import axios from "axios"; // Make sure to import axios
+
+export const getAllListing = async () => {
+  try {
+    const response = await axiosInstance.get("/listings");
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      // Log the error for debugging
+      console.error("Error Listing:", error);
+      throw new Error(error.response?.data?.message || "Listing failed!");
+    } else {
+      console.error("Unexpected error:", error);
+      throw new Error("Listing failed");
+    }
+  }
+};
